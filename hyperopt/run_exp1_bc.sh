e3af1dbe0a190348f480e91fff0471ee3e4b9a85
Oleg Arenz <arenz@ias.tu-darmstadt.de>
2022-11-29 17:40:43 +0100

    fixed for sweep scripts

--- hyperopt/run_exp1_bc.sh
+++ hyperopt/run_exp1_bc.sh
@@ -3,5 +3,6 @@ cd "$(dirname "$0")" # Set CWD to script directory
 
 NUM_WORKERS=16
 CORES_PER_WORKER=6
-for fname in `ls "configs/experiments/exp1 (update stability for single gaussians)/breast_cancer/"`; do ./run_sweep.sh $fname; sleep 1s ; done
+
+for fname in "configs/experiments/exp1 (update stability for single gaussians)/breast_cancer"/*.yml; do NUM_WORKERS=$NUM_WORKERS CORES_PER_WORKER=$CORES_PER_WORKER ./run_sweep.sh "$fname" -t 2-00:00:00; sleep 1s ; done
 
--- hyperopt/run_sweep.sh
+++ hyperopt/run_sweep.sh
@@ -4,11 +4,18 @@ cd "$(dirname "$0")" # Set CWD to script directory
 # We assume that some arguments are "passed" by setting the following environment variables NUM_WORKERS, CORES_PER_WORKER
 # Example usage `NUM_WORKERS=16 CORES_PER_WORKER=6 ./run_sweep ../path/to/sweep_config.yml`
 
-SWEEP_CMD=$(wandb sweep $1 2>&1 | tail -1 | cut -c 29-)
+echo $NUM_WORKERS
+echo $CORES_PER_WORKER
+
+WANDB_MSG=($(wandb sweep "$1" 2>&1))
+echo $WANDB_MSG
+SWEEP_CMD=${WANDB_MSG[@]: -3}
 
 echo running sweep with $SWEEP_CMD
 
-CONFIG_NAME=`basename $1 .yml` 
+echo "basename $1 .yml"
+
+CONFIG_NAME=`basename "$1" .yml` 
 SWEEP_NAME=`echo $SWEEP_CMD | awk -F"/" '{print $NF}'`
 
 GENERATED_SBATCH=`mktemp ${SWEEP_NAME}_${CONFIG_NAME}_XXXX.sbatch -p generated/`
@@ -16,6 +23,6 @@ GENERATED_SBATCH=`mktemp ${SWEEP_NAME}_${CONFIG_NAME}_XXXX.sbatch -p generated/`
 sed -e "s/NUM_WORKERS/${NUM_WORKERS}/g" -e "s;SWEEP_CMD;${SWEEP_CMD};g" -e "s/CORES_PER_WORKER/${CORES_PER_WORKER}/g" wandb_sweep_slurm.sbatch-template > $GENERATED_SBATCH
 
 echo generated sbatch file: $GENERATED_SBATCH
-# sbatch ${@:2} $GENERATED_SBATCH
+sbatch ${@:2} $GENERATED_SBATCH
 
 echo $1 $SWEEP_NAME >> sweep_names.txt
