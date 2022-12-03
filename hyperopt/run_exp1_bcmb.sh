81dc1e6de2f5d219611e81f1aec4219594ca2940
Oleg Arenz <arenz@ias.tu-darmstadt.de>
2022-12-03 10:25:08 +0100

    pushed actual bcmb scripts

---
+++ hyperopt/run_exp1_bcmb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$0")" # Set CWD to script directory 
+
+NUM_WORKERS=16
+CORES_PER_WORKER=6
+
+for fname in "configs/experiments/exp1 (update stability for single gaussians)/breast_cancer_mb"/*.yml; do NUM_WORKERS=$NUM_WORKERS CORES_PER_WORKER=$CORES_PER_WORKER ./run_sweep.sh "$fname" -t 2-00:00:00; sleep 1s ; done
+
--- hyperopt/run_sweep.sh
+++ hyperopt/run_sweep.sh
@@ -4,16 +4,14 @@ cd "$(dirname "$0")" # Set CWD to script directory
 # We assume that some arguments are "passed" by setting the following environment variables NUM_WORKERS, CORES_PER_WORKER
 # Example usage `NUM_WORKERS=16 CORES_PER_WORKER=6 ./run_sweep ../path/to/sweep_config.yml`
 
-echo $NUM_WORKERS
-echo $CORES_PER_WORKER
+#echo $NUM_WORKERS
+#echo $CORES_PER_WORKER
 
 WANDB_MSG=($(wandb sweep "$1" 2>&1))
 echo $WANDB_MSG
 SWEEP_CMD=${WANDB_MSG[@]: -3}
 
 echo running sweep with $SWEEP_CMD
-
-echo "basename $1 .yml"
 
 CONFIG_NAME=`basename "$1" .yml` 
 SWEEP_NAME=`echo $SWEEP_CMD | awk -F"/" '{print $NF}'`
