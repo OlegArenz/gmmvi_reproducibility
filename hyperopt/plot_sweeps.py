9e8c2e961cfc5ebcdbed2af7487e30216cc25736
Oleg Arenz <oleg@robot-learning.de>
2022-12-06 10:34:28 +0100

    updated evaluation script

--- hyperopt/plot_sweeps.py
+++ hyperopt/plot_sweeps.py
@@ -42,22 +42,36 @@ def get_best_elbos(experiments):
         experiment_path = os.path.join(data_path, experiment)
         best_elbos = dict()
         for algorithm_dir in sorted(os.listdir(experiment_path)):
+            all_sweeps = pd.read_csv(os.path.join(experiment_path, algorithm_dir, 'all_sweeps.csv'))
             best_run = os.path.join(experiment_path, algorithm_dir, 'best_runs', '0.csv')
-            best_elbos.update({algorithm_dir: pd.read_csv(best_run)['-elbo'].values[-1]})
+            best_elbo = pd.read_csv(best_run)['-elbo'].values[-1]
+            config = all_sweeps[all_sweeps["-elbos"] == best_elbo]['config']
+            if len(config) != 1:
+                # We recover the config by searching for the final elbo in all_sweeps.csv.
+                # If this failed, we could still get it from wandb
+                print(f"could not recover config for best run with elbo {best_elbo}")
+                best_elbos.update({algorithm_dir: [ best_elbo, config.values[0]] })
+            else:
+                best_elbos.update({algorithm_dir: [ best_elbo, config.values[0]] })
+
         best_elbos_per_experiment.update({experiment: best_elbos})
     return best_elbos_per_experiment
 
-def best_elbo_per_choice(experiments, choices, reduction=np.min):
+def best_elbo_per_choice(experiments, choices):
     best_elbos = get_best_elbos(experiments)
     for experiment in experiments:
+        print(f"processing experiment {experiment}")
         best_elbos_per_experiment = best_elbos[experiment]
         for choice in choices:
-            best_elbos_this_choice = [value for key, value in best_elbos_per_experiment.items() if choice in key]
-            print(f"Choice: {choice} \t Best Elbos: {reduction(best_elbos_this_choice)}")
+            best_candidate_this_choice = sorted([value for key, value in best_elbos_per_experiment.items() if choice in key])[0]
+            print(f"Choice: {choice} \t Best Elbo: {best_candidate_this_choice[0]:.2f} \t "
+                  f"config: {best_candidate_this_choice[1]}")
 
 
 
 if __name__ == "__main__":
-    best_elbos = best_elbo_per_choice(["gmmvi_exp1_wine"], "siytfdr", reduction=np.mean)
+    best_elbos = best_elbo_per_choice(["gmmvi_exp1_wine", "gmmvi_exp1_bc", "gmmvi_exp1_bcmb"], "siytfdr")
+
+
     plot_list(["gmmvi_exp1_wine/septrux", "gmmvi_exp1_wine/sepyrux", "gmmvi_exp1_wine/sepirux"])
     print("done")
