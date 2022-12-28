def93d73b21273dfa5eb0973240e77b1f5e1f8b0
Oleg Arenz <arenz@ias.tu-darmstadt.de>
2022-12-28 07:05:48 +0100

    exp3_bc scripts

--- evaluations/run_exp3_bc.sh
+++ evaluations/run_exp3_bc.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-# This script creates a temporary copy of every yml file with a modified slurm time-limit and run it with clusterwork
-# First argument is the new timelimit in slurm format, Second argument is the experiment that should be run
-for file in configs/exp3\ \(hyperopt\)/*
-  do mkdir -p "tmp/"
-  CONFFILE=bc_`basename "$file"`
-  cp "$file" tmp/$CONFFILE
-  sed -i "tmp/$CONFFILE" -e s/2-00:00:00/$1/
-  python clusterwork.py $CONFFILE -e $2 -s
-done
+./run_conf_with_different_timelimit.sh 0-02:00:00 BC
+
