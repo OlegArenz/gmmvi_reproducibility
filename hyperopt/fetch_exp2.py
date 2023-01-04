6693f5600b637e69c88fb6443bb292931a46edd8
Oleg Arenz <oleg@robot-learning.de>
2023-01-04 19:05:29 +0100

    STM 20 hyperopt scripts

--- hyperopt/fetch_exp2.py
+++ hyperopt/fetch_exp2.py
@@ -1,9 +1,14 @@
-from fetch_from_wandb import process_sweep
-
+from sweep_fetching import fetch_sweeps
 
 if __name__ == "__main__":
+    fetch_sweeps(group_name="joa", project_names=["exp2_stm"],
+                  sweep_names=["septrun", "septron", "septrux", "septrox", "septrug", "septrog",
+                               "semtrun", "semtron", "semtrux", "semtrox", "semtrug", "semtrog",
+                               "saptrun", "saptron", "saptrux", "saptrox", "saptrug", "saptrog",
+                               "samtrun", "samtron", "samtrux", "samtrox", "samtrug", "samtrog"
+                               ])
 
-    process_sweep(group_name="joa", project_names=["exp2_gmm"],
+    fetch_sweeps(group_name="joa", project_names=["exp2_gmm"],
                   sweep_names=["septrun", "septron", "septrux", "septrox", "septrug", "septrog",
                                "semtrun", "semtron", "semtrux", "semtrox", "semtrug", "semtrog",
                                "saptrun", "saptron", "saptrux", "saptrox", "saptrug", "saptrog",
