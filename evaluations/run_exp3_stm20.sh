9111883db1048f0a4a749cdbd3147b2eaa30d255
Oleg Arenz <oleg@robot-learning.de>
2023-01-05 11:34:30 +0100

    fixed stm20 hyperopt scripts

--- evaluations/run_exp3_stm20.sh
+++ evaluations/run_exp3_stm20.sh
@@ -1,4 +1,4 @@
 for file in configs/exp3\ \(hyperopt\)/*
 do
-  ./run_conf_with_different_timelimit.sh "$file" 0-01:00:00 STM20 4
+  ./run_conf_with_different_timelimit.sh "$file" 0-02:00:00 STM20 4
 done
