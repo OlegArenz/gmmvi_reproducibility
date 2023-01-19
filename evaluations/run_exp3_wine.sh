074b6ad7513cd53c6dfcc909dc2e693cce101515
Oleg Arenz <arenz@ias.tu-darmstadt.de>
2023-01-19 11:09:21 +0100

    fixed wine script

--- evaluations/run_exp3_wine.sh
+++ evaluations/run_exp3_wine.sh
@@ -1,4 +1,4 @@
 for file in configs/exp3\ \(hyperopt\)/samtron*
 do
-  ./run_conf_with_different_timelimit.sh "$file" 1-00:00:00 STM300 12
+  ./run_conf_with_different_timelimit.sh "$file" 1-00:00:00 WINE 12
 done
