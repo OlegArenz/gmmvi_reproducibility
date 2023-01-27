96dce3faf806e73fb5cb350009aecfc40a4e0326
Oleg Arenz <oleg@robot-learning.de>
2023-01-27 09:15:28 +0100

    talos memtest 2

--- evaluations/run_exp3_talos.sh
+++ evaluations/run_exp3_talos.sh
@@ -1,4 +1,4 @@
 for file in configs/exp3\ \(hyperopt\)/samtron*
 do
-  ./run_conf_with_different_timelimit.sh "$file" 1-00:00:00 TALOS 4
+  ./run_conf_with_different_timelimit.sh "$file" 1-00:00:00 TALOS 12
 done
