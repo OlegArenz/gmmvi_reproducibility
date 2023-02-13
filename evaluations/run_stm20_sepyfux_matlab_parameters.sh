7e32ae637d3a3dae3a36b9ecb84259c5053b3b3b
Oleg Arenz <oleg@robot-learning.de>
2023-02-13 08:52:30 +0100

    new matlab-param evaluation

--- evaluations/run_stm20_sepyfux_matlab_parameters.sh
+++ evaluations/run_stm20_sepyfux_matlab_parameters.sh
@@ -1,4 +1,4 @@
 for file in configs/matlab_comparison/*
 do
-  ./run_conf_with_different_timelimit.sh "$file" 0-02:00:00 STM20 4
+  ./run_conf_with_different_timelimit.sh "$file" 1-00:00:00 STM20 4
 done
