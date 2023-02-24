97c32392c40521e5d23470bc8c37606b094985bc
Oleg Arenz <arenz@ias.tu-darmstadt.de>
2023-02-24 09:34:13 +0100

    synced some runscripts

--- evaluations/run_exp3_p4.sh
+++ evaluations/run_exp3_p4.sh
@@ -1,4 +1,4 @@
 for file in configs/exp3\ \(hyperopt\)/*
 do
-  ./run_conf_with_different_timelimit.sh "$file" 0-08:00:00 Planar4 4
+  ./run_conf_with_different_timelimit.sh "$file" 0-08:00:00 Planar4 6
 done
